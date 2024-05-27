d671ad7d7fb645fd7bf3d0304988d66f94036e6d
OleksChernikov <pjatochkin2013@gmail.com>
2024-05-27 13:35:42 +0300

    make build

---
+++ index.247b22c3.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("body--with-menu"):document.body.classList.remove("body--with-menu")});
+//# sourceMappingURL=index.247b22c3.js.map
